42e611c70ea56bb7acf4012a35daedb844db950f
Max Kuttner <maxl.kuttner@gmail.com>
2020-04-05 19:32:09 +0200

    initial commit

---
+++ ex1.py
@@ -0,0 +1,65 @@
+from pandas_datareader import data
+start_date = '2014-01-01'
+end_date = '2018-01-01'
+goog_data = data.DataReader('GOOG', 'yahoo', start_date, end_date)
+
+#asdf
+import numpy as np
+import pandas as pd
+
+goog_data_signal = pd.DataFrame(index=goog_data.index)
+goog_data_signal['price'] = goog_data['Adj Close']
+goog_data_signal['daily_difference'] = goog_data_signal['price'].diff()
+goog_data_signal['signal'] = 0.0
+goog_data_signal['signal'] = np.where(goog_data_signal['daily_difference'] > 0, 1.0, 0.0)
+
+goog_data_signal['positions'] = goog_data_signal['signal'].diff()
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax1 = fig.add_subplot(111, ylabel='Google price in $')
+goog_data_signal['price'].plot(ax=ax1, color='b', lw=2.)
+
+
+ax1.plot(goog_data_signal.loc[goog_data_signal.positions == 1.0].index,
+         goog_data_signal.price[goog_data_signal.positions == 1.0],
+         '^', markersize=5, color='green')
+
+ax1.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index,
+         goog_data_signal.price[goog_data_signal.positions == -1.0],
+         'v', markersize=5, color='red')
+
+plt.show()
+
+from pandas_datareader import data
+start_date = '2014-01-01'
+end_date = '2018-01-01'
+goog_data = data.DataReader('GOOG', 'yahoo', start_date, end_date)
+
+#asdf
+import numpy as np
+import pandas as pd
+
+goog_data_signal = pd.DataFrame(index=goog_data.index)
+goog_data_signal['price'] = goog_data['Adj Close']
+goog_data_signal['daily_difference'] = goog_data_signal['price'].diff()
+goog_data_signal['signal'] = 0.0
+goog_data_signal['signal'] = np.where(goog_data_signal['daily_difference'] > 0, 1.0, 0.0)
+
+goog_data_signal['positions'] = goog_data_signal['signal'].diff()
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax1 = fig.add_subplot(111, ylabel='Google price in $')
+goog_data_signal['price'].plot(ax=ax1, color='b', lw=2.)
+
+
+ax1.plot(goog_data_signal.loc[goog_data_signal.positions == 1.0].index,
+         goog_data_signal.price[goog_data_signal.positions == 1.0],
+         '^', markersize=5, color='green')
+
+ax1.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index,
+         goog_data_signal.price[goog_data_signal.positions == -1.0],
+         'v', markersize=5, color='red')
+
+plt.show()
